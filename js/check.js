617db54f79adaf5955da4ca109388c183be3b81b
Jari Huilla <jari.huilla@gmail.com>
2015-02-25 22:04:08 +0200

    Refactor copy-paste out of sum.js

--- js/check.js
+++ js/check.js
@@ -8,7 +8,7 @@ $('#getstarted input[type="checkbox"]').each(function(index) {
         var description = $(this).siblings('label').first().find('.labelautyLabel').first().html().trim();
 
         if ($(this).prop('checked')) {
-            $("#summary-table").append('<tr id="result'+index+'" class="result"> <td class="col-md-4"> <h6>'+description+'</h6> </td> <td> <input id="p'+index+'" name="p'+index+'" type="number"  value="0" class="col-md-2 form-control"> </td> <td> <input  id="d'+index+'" name="d'+index+'" type="number"  value="0" class="col-md-2 form-control"> </td> <td> <input  id="c'+index+'" name="c'+index+'" type="number" disabled value="0" class="col-md-4 form-control"> </td> </tr>');
+            $("#summary-table").append('<tr id="result'+index+'" class="result"> <td class="col-md-4"> <h6>'+description+'</h6> </td> <td> <input id="p'+index+'" name="p'+index+'" type="number"  value="0" class="p col-md-2 form-control"> </td> <td> <input  id="d'+index+'" name="d'+index+'" type="number"  value="0" class="d col-md-2 form-control"> </td> <td> <input  id="c'+index+'" name="c'+index+'" type="number" disabled value="0" class="c col-md-4 form-control"> </td> </tr>');
             //START ONBLUR
             $('input').blur(function(){
             $('input[name=c'+index+']').val( +($('input[name=p'+index+']').val()) *(+ $('input[name=d'+index+']').val()) );
